14598a0bedec0ab5b930e7bd5b7e58325f7b8949
Pierre Stainton <pierre.stainton@gmail.com>
2017-02-25 14:00:50 +0100

    Création du projet

---
+++ src/main/java/com/hardis/google/hashcode/videos/App.java
@@ -0,0 +1,17 @@
+package com.hardis.google.hashcode.videos;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Created by pierrestainton on 25/02/2017.
+ */
+public class App {
+
+    final static Logger LOGGER = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Lancement du programme.");
+    }
+
+}
